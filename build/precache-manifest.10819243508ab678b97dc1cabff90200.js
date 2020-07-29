4fe199ad4e3bd1b38492d820dc59b5ac050bc848
anne.a.mery <anne.a.mery@renault.com>
2020-07-29 12:43:34 +0200

    demarrage

---
+++ build/precache-manifest.10819243508ab678b97dc1cabff90200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8edb143dbf3614edec350c9be30448",
+    "url": "/92mery/chef-doeuvre/index.html"
+  },
+  {
+    "revision": "a4754a515a3950003efd",
+    "url": "/92mery/chef-doeuvre/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "caa9aeed462219f26faf",
+    "url": "/92mery/chef-doeuvre/static/js/2.bed78e95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/92mery/chef-doeuvre/static/js/2.bed78e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4754a515a3950003efd",
+    "url": "/92mery/chef-doeuvre/static/js/main.ac26ddb4.chunk.js"
+  },
+  {
+    "revision": "4b5aabda4ee442055e43",
+    "url": "/92mery/chef-doeuvre/static/js/runtime-main.2820706f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/92mery/chef-doeuvre/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './Home';
+import NavBar from './NavBar';
+
+function App() {
+  return    <div>
+      <NavBar/>      
+      <Home />
+    </div>;
+    
+}
+
+export default App;
